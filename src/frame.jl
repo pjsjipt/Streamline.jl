aa0a05e0357cf5b33b603feab51dce2528734f01
= <pjabardo@ipt.br>
2025-09-03 19:24:30 -0300

    TRabalhando na interface.
    Ainda está uma merda e pouco confiável

--- src/frame.jl
+++ src/frame.jl
@@ -1,4 +1,10 @@
+import Polynomials: Polynomial
 
-
+export pressure, temperature, HWFrame
 mutable struct HWFrame
+    cal_t::Polynomial{Float64,:T}
+    cal_p::Polynomial{Float64,:P}
 end
+
+Hotwire.pressure(fr::HWFrame, P) = fr.cal_p(P*1000)
+Hotwire.temperature(fr::HWFrame, T) = fr.cal_t(T)
